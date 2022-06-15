14ced423aa6af9c2309eba01c9076838df71f7e5
SolenMakonnen <SolenMakonnen@192.168.0.12>
2022-06-14 20:06:48 -0400

    Created class objects

--- BlackJack/src/BlackJack.java
+++ BlackJack/src/BlackJack.java
@@ -1,8 +1,15 @@
 
 public class BlackJack {
-
+	
+	private Deck deck;
+	private Dealer dealer;
+	private Player player;
 
 	public BlackJack() {
+		//class objects
+		deck= new Deck();
+		dealer= new Dealer();
+		player= new Player();
 		
 	}
 	
