dcd8189ec8f794063a7383c4edf48a93cff11351
solenm <solenm@gmail.com>
2022-06-15 14:09:44 -0400

    added enumerations for suit and card type

--- BlackJack/src/Card.java
+++ BlackJack/src/Card.java
@@ -1,4 +1,22 @@
 
+
 public class Card {
 
+	
+	/**
+	 * Suits of a card deck
+	 */
+	public static enum Suit{
+		CLUB, DIAMOND, HEART, SPADE
+	}
+	
+	/**
+	 * Types of cards
+	 */
+	public static enum Type {
+		ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+	}
+
+	private Suit suit;
+	private Type type;
 }
