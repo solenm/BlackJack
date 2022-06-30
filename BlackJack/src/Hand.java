bef3fb302ce4d222f17d4f5f8ea893bdca94ee8e
solenm <solenm@gmail.com>
2022-06-30 14:03:19 -0400

    added constructor

--- BlackJack/src/Hand.java
+++ BlackJack/src/Hand.java
@@ -1,4 +1,23 @@
-
+/**
+ * 
+ * @author ghyzel
+ *
+ */
 public class Hand {
+	
+	/**
+	 * The cards in the hand
+	 */
+	private Card [] cards;
+	
+	/**
+	 * Creates an empty hand
+	 */
+	public Hand() {
+		//Initializing an empty array. 
+		//Calling cards.length on this array would return 0
+		cards = new Card[0];		
+	}
+
 
 }
