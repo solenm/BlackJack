cf8d5fa6140877f31696d663a40ca8bd84bd0f39
solenm <solenm@gmail.com>
2022-07-01 17:03:19 -0400

    get method for value of the card

--- BlackJack/src/BlackJack.java
+++ BlackJack/src/BlackJack.java
@@ -21,6 +21,58 @@ public class BlackJack {
 	}
 	
 	/**
+	 * Returns the value of a card in a standard game of Blackjack based on the type of the card
+	 * ex. An Ace would return 1, a 2 would return 2... 
+	 * @param c card whose value is extracted
+	 * @return value of the card
+	 */
+	public static int getValueOfCard(Card c) {
+		//if and else if statements for card values 
+		if(Card.Type.ACE == c.getType())
+		{
+			return 1;
+		}
+		else if(Card.Type.TWO == c.getType())
+		{
+			return 2;
+		}
+		else if(Card.Type.THREE == c.getType())
+		{
+			return 3;
+		}
+		else if(Card.Type.FOUR == c.getType())
+		{
+			return 4;
+		}
+		else if(Card.Type.FIVE == c.getType())
+		{
+			return 5;
+		}
+		else if(Card.Type.SIX == c.getType())
+		{
+			return 6;
+		}
+		else if(Card.Type.SEVEN == c.getType())
+		{
+			return 7;
+		}
+		else if(Card.Type.EIGHT == c.getType())
+		{
+			return 8;
+		}
+		else if(Card.Type.NINE == c.getType())
+		{
+			return 9;
+		}
+		else
+		{
+			return 10;
+		}
+		
+		
+	}
+	
+	/**
 	 * @return Deck used to play
 	 */
 	public Deck getDeck() {
