fcf8679b7d590ef0d73ff72dbffc2a9eb0b2333e
solenm <solenm@gmail.com>
2022-07-01 12:17:38 -0400

    default constructor

--- BlackJack/src/Dealer.java
+++ BlackJack/src/Dealer.java
@@ -1,4 +1,17 @@
 
 public class Dealer {
+	/**
+	 * The dealers hand
+	 */
+	Hand hand;
+	
+	/**
+	 * Construct a new dealer with an empty hand
+	 */
+	public Dealer() {
+		hand= new Hand();
+		
+	}
+	
 
 }
