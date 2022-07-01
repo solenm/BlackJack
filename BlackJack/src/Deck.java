3212c439b5e18647873e90143cdb0520f5a99d3e
solenm <solenm@gmail.com>
2022-07-01 12:33:50 -0400

    deck constructor

--- BlackJack/src/Deck.java
+++ BlackJack/src/Deck.java
@@ -1,4 +1,34 @@
-
+import java.util.Random;
 public class Deck {
+	/**
+	 * Cards in the deck
+	 */
+	private Card [] cards;
+	
+	/**
+	 * The number of cards in the physical deck on the table.
+	 */
+	private int numCardsInDeck;
+	
+	/**
+	 * Create a standard 52 card deck w/ one of each type (Ace, King, Queen...) for each Suit (Hearts, Spades ..)
+	 */
+	public Deck() {
+
+		Card.Suit [] suits = Card.Suit.values(); //creates an array of all of the Suits
+		Card.Type [] types = Card.Type.values(); //creates an array of all of the Types
+		cards = new Card[suits.length*types.length]; //sets the size of cards to 52 (types & suits)
+		numCardsInDeck = 0;
+
+		for(int i =0; i<suits.length; i++)
+		{
+			for(int j = 0; j<types.length; j++)
+			{
+				//Assigns 52 unique cards to the cards deck
+				cards[numCardsInDeck] = new Card(types[j], suits[i]); 
+				numCardsInDeck++; //increments the number of cards in the deck
+			}
+		}	
+	}
 
 }
