00dd534b1fad3edc28fb88fb9afa20f7532819fc
solenm <solenm@gmail.com>
2022-07-01 18:58:56 -0400

    initialize variables

--- BlackJack/src/BlackJackApp.java
+++ BlackJack/src/BlackJackApp.java
@@ -20,9 +20,36 @@ public class BlackJackApp extends Application {
 	StackPane root;
 	ImageView [] playerCards;
 	ImageView [] dealerCards;
+	AudioClip cardFlip, clapping;
+	ImageView hole;
+	ImageView hit, stay, quit;
+	Image hitUp, hitDown, stayUp, stayDown;
+	int round, playerWins, dealerWins;
+	Text dealerWinsText, playerWinsText, playerScoreText, dealerScoreText;
+	boolean playerTurn;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		// TODO Auto-generated method stub
+		game = new BlackJack();
+		playerCards = new ImageView [5];
+		dealerCards = new ImageView [5];
+		dealerWinsText = new Text(0, 0, "");
+		dealerWinsText.setFill(Color.WHITE);
+		dealerWinsText.setFont(Font.font("Verdana", 20));
+		dealerWinsText.setTranslateX(250);
+		dealerWinsText.setTranslateY(225);
+		playerWinsText = new Text(0, 0, "");
+		playerWinsText.setFill(Color.WHITE);
+		playerWinsText.setFont(Font.font("Verdana", 20));
+		playerWinsText.setTranslateX(250);
+		playerWinsText.setTranslateY(190);
+		playerScoreText = new Text(0, 0, "");
+		playerScoreText.setFill(Color.WHITE);
+		playerScoreText.setFont(Font.font("Verdana", 20));
+		playerScoreText.setTranslateX(-250);
+		playerScoreText.setTranslateY(-190);
+		cardFlip = new AudioClip("file:classic-cards/cardPlace1.wav");
+		clapping = new AudioClip("file:classic-cards/wellDone.wav");
 		
 	}
 }
