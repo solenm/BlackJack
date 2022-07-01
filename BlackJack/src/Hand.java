9b7d7fa19fc6f5a6e662fb66b44aef236062c50d
solenm <solenm@gmail.com>
2022-07-01 11:31:55 -0400

    added addCard method

--- BlackJack/src/Hand.java
+++ BlackJack/src/Hand.java
@@ -19,5 +19,23 @@ public class Hand {
 		cards = new Card[0];		
 	}
 
+	/**
+	 * Adds Card c to the hand
+	 * 
+	 * @param c card to be added
+	 */
+	public void addCard(Card c) {
+		//create deep copy of array
+		Card [] array = new Card[cards.length + 1];
+		//for loop to fill array with same contents
+		for(int z= 0; z < array.length-1; z++)
+		{
+			array[z]= cards[z];
+		}
+		//equal array to last card to drawn card
+		array[array.length-1]=c;
+		cards= array;
+		
+	}
 
 }
