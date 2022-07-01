868278944d2dc5a33d4a16b70b2d3378ebf12069
solenm <solenm@gmail.com>
2022-07-01 16:21:55 -0400

    get methods

--- BlackJack/src/BlackJack.java
+++ BlackJack/src/BlackJack.java
@@ -10,7 +10,30 @@ public class BlackJack {
 		deck= new Deck();
 		dealer= new Dealer();
 		player= new Player();
+		//shuffle
+		deck.shuffle();
+		//player & dealer hands
 		
 	}
 	
+	/**
+	 * @return Deck used to play
+	 */
+	public Deck getDeck() {
+		return deck;
+	}
+	
+	/**
+	 * @return Dealer of the game
+	 */
+	public Dealer getDealer() {
+		return dealer;
+	}
+	
+	/**
+	 * @return Player playing the blackjack game
+	 */
+	public Player getPlayer() {
+		return player;
+	}
 }
