f0da108665058b733f0f9cc926fd630258a1d07e
solenm <solenm@gmail.com>
2022-07-01 18:34:06 -0400

    created BlackJackApp class

---
+++ BlackJack/src/BlackJackApp.java
@@ -0,0 +1,28 @@
+import javafx.animation.Animation;
+import javafx.animation.Transition;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.AudioClip;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
+
+public class BlackJackApp extends Application {
+	BlackJack game;
+	StackPane root;
+	ImageView [] playerCards;
+	ImageView [] dealerCards;
+	@Override
+	public void start(Stage primaryStage) {
+		// TODO Auto-generated method stub
+		
+	}
+}
