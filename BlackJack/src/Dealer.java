716847ca6670e7c43da7e1c1cae9d657151f8ff6
solenm <solenm@gmail.com>
2022-07-01 18:06:59 -0400

    get hand and play turn methods

--- BlackJack/src/Dealer.java
+++ BlackJack/src/Dealer.java
@@ -13,5 +13,32 @@ public class Dealer {
 		
 	}
 	
+	/**
+	 * Dealer draws a card if his hand is worth less than 17 points and has less than 5 cards in in his hand.
+	 * 
+	 * @param deck
+	 * @return
+	 */
+	public Card playTurn(Deck deck) {
+		Card holder;
+		if( BlackJack.getValueOfHand(hand) < 17 && hand.getCards().length < 5)
+		{
+			//draw cards and add them to the hand
+			holder= deck.draw();
+			hand.addCard(holder);
+			return holder;
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
+	/**
+	 * @return Hand the dealer's hand
+	 */
+	public Hand getHand() {
+		return hand;
+	}
 
 }
