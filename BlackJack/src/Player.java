864204611f61dfe787143b67aa5fad858bd65f28
solenm <solenm@gmail.com>
2022-07-01 16:25:10 -0400

    constructor added

--- BlackJack/src/Player.java
+++ BlackJack/src/Player.java
@@ -1,4 +1,16 @@
 
 public class Player {
+	/**
+	 * The players hand
+	 */
+	private Hand hand;
+	
+	/**
+	 * Instantiates the hand instance variable.
+	 */
+	public Player() {
+		hand= new Hand();
+	}
+	
 
 }
