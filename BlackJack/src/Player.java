40f0f572244089d293bc6d25414a217c887c9733
solenm <solenm@gmail.com>
2022-07-01 17:22:54 -0400

    card return and car value check

--- BlackJack/src/Player.java
+++ BlackJack/src/Player.java
@@ -12,6 +12,32 @@ public class Player {
 		hand= new Hand();
 	}
 	
+	/**
+	 * @return true if the player has bused
+	 */
+	public boolean busted() {
+		if(BlackJack.getValueOfHand(hand) > 21)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	/**
+	 * empty the player hand into the cards
+	 * @param d deck that receives the cards
+	 */
+	public void returnCardstoDeck(Deck d) {
+		//loop to cards to deck and empty hand
+		for(int count= 0; count < hand.getCards().length; count++)
+		{
+			d.addToBottom(hand.getCards()[count]);
+		}
+		hand.emptyHand();
+	}
 	
 	/**
 	 * @return the player's hand
