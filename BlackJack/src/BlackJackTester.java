88a04025500f08d43fae093dc928d331c7b9bbff
solenm <solenm@gmail.com>
2022-07-05 12:39:07 -0400

    class creation

--- BlackJack/src/BlackJackTester.java
+++ BlackJack/src/BlackJackTester.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 
-public class BlackJackTester {
+
+public class BlackJackTester extends BlackJackApp{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
